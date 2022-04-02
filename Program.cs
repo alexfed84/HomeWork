2968466935df581124339c3d2e853623a9b20232
alexfed84 <alfed09@gmail.com>
2022-04-02 17:05:58 +0300

    code_1

--- Program.cs
+++ Program.cs
@@ -1,12 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 string[] array = new string[5] {"city", "qx", "hello", "world", "res"};
-int count = 0;
-    for (int i = 0; i < array.Length; i++)
+for (int i = 0; i < array.Length; i++)
     {
-    if(array[i].Length <= 3)
-        {
-        array[count] = array[i];
-        count++;
-        }
+        if (array[i].Length <= 3);
     }
-Console.Write($"{array[i]} ");
+     Console.Write($"{array} ");
+
+
+
