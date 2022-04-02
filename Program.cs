4a5c447dd7f2d6170ceeb24e5e643f70e651ce4d
alexfed84 <alfed09@gmail.com>
2022-04-02 16:00:03 +0300

    Create .gitignore

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] array = new string[5] {"city", "qx", "hello", "world", "res"};
+int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        array[count] = array[i];
+        count++;
+        }
+    }
+Console.Write($"{array[i]} ");
