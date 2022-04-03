a12559895fe6658285b0e81e4f8797a14ec1652a
alexfed84 <alfed09@gmail.com>
2022-04-03 10:41:10 +0300

    correction_program_1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
 ﻿
 string[] array = new string[5] {"city", "qx", "hello", "world", "res"};
+string[] array1 = new string[array.Length];
+int res = 0;
 for (int i = 0; i < array.Length; i++)
     {
         if (array[i].Length <= 3);
+        {
+        array1[res] = array[i];
+        res++;
+        }
     }
-     Console.Write($"{array} ");
+Console.Write($"{res} ");
 
 
 
